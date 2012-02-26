c388612690c33a6b20a9b3dab4e2aa1ac6cd6a32
Adam Rezich <adam@rezich.com>
2012-02-26 04:32:19 -0600

    moved a buncha files around

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
-var url = require('url');
-
-var html = require('fs').readFileSync(__dirname+'/index.html');
-var css = require('fs').readFileSync(__dirname+'/style.css');
-var engine = require('fs').readFileSync(__dirname+'/engine.js');
-var game = require('fs').readFileSync(__dirname+'/game.js');
-var fov = require('fs').readFileSync(__dirname+'/fov.js');
-var jquery = require('fs').readFileSync(__dirname+'/jquery-1.7.1.min.js');
-var server = require('http').createServer(function(req, res) {
-	switch(url.parse(req.url).pathname) {
-		case '/': res.end(html); break;
-		case '/style.css': res.end(css); break;
-		case '/jquery-1.7.1.min.js': res.end(jquery); break;
-		case '/fov.js': res.end(fov); break;
-		case '/engine.js': res.end(engine); break;
-		case '/game.js': res.end(game); break;
-		default: res.end(""); break;
-	}
+var static = require('node-static');
+var http = require('http');
+var util = require('util');
+var webroot = '.';
+var port = 8080;
+var file = new (static.Server)(webroot, {
+	cache: 600
 });
-server.listen(1337);
+var server = http.createServer(function (req, res) {
+	req.addListener('end', function () {
+		file.serve(req, res, function (err, result) {
+			if (err) {
+				console.error('Error serving %s - %s', req.url, err.message);
+				if (err.status === 404 || err.status === 500) {
+					file.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
+				}
+				else {
+					res.writeHead(err.status, err.headers);
+					res.end();
+				}
+			}
+			else {
+				console.log('%s - %s', req.url, res.message);
+			}
+		});
+	});
+}).listen(port);
 
 var nowjs = require("now");
 var everyone = nowjs.initialize(server);
